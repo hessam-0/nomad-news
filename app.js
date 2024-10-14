89cf456b6515b8eeb07e5e6f475931171e2f15c2
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-14 18:02:40 +0100

    update app.js with endpoints

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const apiRouter = require("./routes/api-routers");
 const app = express();
 const { psqlErrorHandler, serverErrorHandler, customErrorHandler } = require("./error-handlers.js");
+const endpoints = require("./endpoints.json");
+
 app.use(express.json());
 app.use("/api", apiRouter);
+
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: 'Not Found' });
