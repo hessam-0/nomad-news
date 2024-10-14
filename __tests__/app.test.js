38070a937dec9879b2d6055f4ceee7b67a4162b2
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-14 17:17:34 +0100

    add test directory and app test file

---
+++ __tests__/app.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const seed = require("../db/seeds/seed.js")
+const db = require("../db/connection.js")
+const data = require("../db/data/test-data/index.js")
+const app = require("../app.js");
+
+beforeEach(() => {
+    return seed(data);
+});
+
+afterAll(() => {
+    db.end();
+});
+
+describe('GET /api/topics', () => {
+    it('Should respond with status 200 an array of topics with slug and description', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((res) => {
+            const { topics } = res.body;
+            expect(Array.isArray(topics)).toBe(true);
+            topics.forEach((topic) => {
+                expect(topic).toEqual({
+                    slug : expect.any(String),
+                    description : expect.any(String)
+                })
+            })
+        })
+    });
+    it('Should respond with a status 404 and error message for invalid endpoints', () => {
+        return request(app)
+        .get('/api/xxx')
+        .expect(404)
+        .then(({ body }) => {
+            expect(body.msg).toBe('Not Found');
+        })
+    })
+});
