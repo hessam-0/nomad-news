ebcd625d5a14c84c80a782c1f2ede486d01477e0
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-15 21:33:48 +0100

    added comments by article_id subrouter mount point here

--- routes/article-router.js
+++ routes/article-router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { getArticleById, getAllArticles } = require("../controllers/article-controller");
+//const commentsRouter = require("./comments-router");
+const { getCommentsByArticleId } = require("../controllers/comments-controller")
 const articleRouter = express.Router();
 
 articleRouter.get('/:article_id', getArticleById);
 articleRouter.get('/', getAllArticles);
+articleRouter.get('/:article_id/comments', getCommentsByArticleId);
 
 module.exports = articleRouter;
