ff08c97985a9f58198c76128e07827ce3e9ed89d
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-15 21:24:30 +0100

    add comments model to find query db for article via id and retreive comments for each article

---
+++ models/comments-model.js
@@ -0,0 +1,16 @@
+const db = require("../db/connection");
+
+exports.fetchCommentsByArticleId = (article_id) => {
+
+  return db.query('SELECT * FROM articles WHERE article_id = $1', [article_id])
+    .then(({ rows: articleRows }) => {
+      if (articleRows.length === 0) {
+        return Promise.reject({ status: 404, msg: 'Article Not Found' });
+      }
+      const query = `SELECT comment_id, votes, created_at, author, body, article_id FROM comments WHERE article_id = $1 ORDER BY created_at DESC`
+      return db.query(query, [article_id])
+    })
+    .then(({ rows }) => {
+      return rows;
+    })
+}
