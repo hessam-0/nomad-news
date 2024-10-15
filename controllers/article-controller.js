86707e15fb27409788c5c5a7f4a3fd58e9bf5fd6
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-15 16:19:37 +0100

    add getAllArticles to the articles controller

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,10 +1,20 @@
-const { fetchArticleById } = require("../models/article-model");
+const { fetchArticleById, fetchAllArticles } = require("../models/article-model");
 
 exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params
     fetchArticleById(article_id)
     .then((article) => {
         res.status(200).send({ article })
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+
+exports.getAllArticles = (req, res, next) => {
+    fetchAllArticles()
+    .then((articles) => {
+        res.status(200).send({ articles });
     })
     .catch((err) => {
         next(err)
