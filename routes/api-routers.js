ab11af6b862f3dd3ee01979a82590d272cfdb4ee
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-15 21:36:26 +0100

    this file was changed to fix routing errors to account for comments endpoint

--- routes/api-routers.js
+++ routes/api-routers.js
@@ -1,6 +1,7 @@
 const apiRouter = require("express").Router();
 const topicsRouter = require("./topics-router");
 const articleRouter = require("./article-router");
+//const commentsRouter = require("./comments-router");
 const endpoints = require("../endpoints.json");
 const { end } = require("../db/connection");
 
@@ -10,5 +11,6 @@ apiRouter.get("/", (req, res) => {
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/articles', articleRouter);
+
 
 module.exports = apiRouter;
