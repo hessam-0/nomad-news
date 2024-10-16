c332ad94a41096139f4a287bf69c9f71b40c533c
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-10-16 10:48:18 +0100

    add updateArticleVotes to article controller

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,4 +1,4 @@
-const { fetchArticleById, fetchAllArticles } = require("../models/article-model");
+const { fetchArticleById, fetchAllArticles, updateArticleVotesById } = require("../models/article-model");
 
 exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params
@@ -19,4 +19,19 @@ exports.getAllArticles = (req, res, next) => {
     .catch((err) => {
         next(err)
     })
+}
+
+exports.updateArticleVotes = (req, res, next) => {
+    const { article_id } = req.params;
+    const { inc_votes } = req.body;
+
+    if(inc_votes === undefined){
+        return next({ status: 400, msg: "Bad Request"})
+    }
+
+    updateArticleVotesById(article_id, inc_votes)
+    .then((updatedArticle) => {
+        res.status(200).json({ article: updatedArticle })
+    })
+    .catch(next)
 }
