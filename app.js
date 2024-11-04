1bf598d9f34b755bb9bf1ef8fa932204069b0e6a
Hesam <64800321+hessam-0@users.noreply.github.com>
2024-11-04 11:02:10 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const apiRouter = require("./routes/api-routers");
 const app = express();
 const { psqlErrorHandler, serverErrorHandler, customErrorHandler } = require("./error-handlers.js");
 const endpoints = require("./endpoints.json");
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
-
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: 'Not Found' });
